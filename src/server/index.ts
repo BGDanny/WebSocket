e8f054995846acc9719c17029f3595744d659df0
Junhao Xue <junhao.xue@justeattakeaway.com>
2021-11-29 14:01:53 -0700

    Add logic for multiple tabs

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+// TODO change to https
 import * as http from "http";
 import { WebSocketServer, WebSocket } from "ws";
 
@@ -6,16 +7,53 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws: WebSocket) => {
-    console.log("New client connection");
-    ws.send("You are now connected to the server");
+interface IUserConnection {
+    [key: string]: {
+        numberOfConnection: number;
+        online: boolean;
+    }
+};
 
-    // ws.on("message", (message) => {
-    //     console.log(message);
-    // });
+let userConnection: IUserConnection = {};
+
+wss.on("connection", (ws: WebSocket) => {
+    console.log(`New client connection from ${ws.url}. User status is default to offline`);
+    ws.send("You are now connected to the server");
+    let username = "";
 
     ws.onmessage = (e) => {
-        console.log(e.data);
+        const payload = JSON.parse(e.data.toString());
+        if (payload.username) {
+            username = payload.username;
+            if (!userConnection[username]) {
+                userConnection = {
+                    ...userConnection,
+                    [username]: {
+                        numberOfConnection: 1,
+                        online: false
+                    },
+                };
+            } else {
+                userConnection[username].numberOfConnection++;
+            }
+        } else if (payload.online !== undefined && userConnection[username].numberOfConnection === 1) {
+            userConnection[username].online = payload.online;
+        }
+        console.log(userConnection);
+    }
+
+    ws.onclose = () => {
+        console.log("One connection lost");
+        if (userConnection[username]) {
+            if (!--userConnection[username].numberOfConnection) {
+                userConnection[username].online = false;
+            }
+        }
+        console.log(userConnection);
+    }
+
+    ws.onerror = (e) => {
+        console.log(e.message);
     }
 });
 
